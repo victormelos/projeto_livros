d716b17a8f65e11df0ea69a75f1bb461a79c22bf
Victor Melo <victormelotrabalho1@gmail.com>
2025-02-19 08:46:30 -0300

    Add PostgreSQL database connection and basic query

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"log"
+)
 
+func main() {
+	db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/postgres")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
 }
